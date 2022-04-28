eb909d1bf252df075c177fde3a37799ea90ab436
ggenilson <genilsonaraujo20002002@gmail.com>
2022-04-28 04:15:16 +0100

    chore: updates

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -8,12 +8,12 @@ class CreateUserController {
   handle(request: Request, response: Response): Response {
     const { name, email } = request.body;
 
-    this.createUserUseCase.execute({
+    const user = this.createUserUseCase.execute({
       name,
       email,
     });
 
-    return response.status(201).send();
+    return response.status(201).send(user);
   }
 }
 
